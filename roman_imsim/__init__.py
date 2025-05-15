66d3f3f9fb503087ef2f765c65070ef6a9353eeb
Troxel <michael.troxel@duke.edu>
2025-05-15 15:24:10 +0200

    updates to include build and flake/black

--- roman_imsim/__init__.py
+++ roman_imsim/__init__.py
@@ -1,14 +1,16 @@
 try:
     from lsst.utils.threads import disable_implicit_threading
+
     disable_implicit_threading()
 except:
     pass
-from .obseq import *
-from .psf import *
-from .sca import *
-from .stamp import *
-from .wcs import *
-from .skycat import *
-from .photonOps import *
-from .bandpass import *
-from .detector_physics import *
+# Import core modules for public use
+from . import bandpass
+from . import wcs
+from . import photonOps
+from . import obseq
+from . import utils
+from . import scafile
+from . import psf
+from . import skycat
+from . import sca
---
+++ tests/test_imports.py
@@ -0,0 +1,21 @@
+import importlib
+import pkgutil
+import roman_imsim
+
+
+def test_all_modules_import():
+    """Recursively try importing all submodules in roman_imsim."""
+    package = roman_imsim
+    errors = []
+
+    for loader, module_name, is_pkg in pkgutil.walk_packages(
+        package.__path__, package.__name__ + "."
+    ):
+        try:
+            importlib.import_module(module_name)
+        except Exception as e:
+            errors.append((module_name, repr(e)))
+
+    assert not errors, f"Failed to import modules:\n" + "\n".join(
+        f"{m}: {e}" for m, e in errors
+    )
