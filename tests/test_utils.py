834cfd65268cf9d2b44eeb15fb234d91fa881d19
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2025-06-25 16:43:50 -0400

    Assert PSF is not None

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,10 @@
-import pytest
-
 from roman_imsim.utils import roman_utils
 
-@pytest.fixture
-def test_getPSF_Image():
-    pass
 
+def test_getPSF_Image():
+    config_path = "../SimHackJune2025/hack.yaml"
+    pointing, sca = 12909, 4
+    config = roman_utils(config_path, pointing, sca)
+    size, x, y = 64, 2048, 2048
+    psf = config.getPSF_Image(size, x, y)
+    assert psf is not None
