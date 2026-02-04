1c157c558a42917549a57d391f73fd2119010f41
Arun Kannawadi <arun.kannawadi@duke.edu>
2026-02-04 12:31:50 -0500

    Updates from Russell
    He did three things:
    1. got the code to work equally well for prism or grism
    2. included a new method to compute the dispersion (ie. dlam/dr, where dr is the path along the trace)cleaned up a few things regarding how they do intermediate trsnforms for the wavelength.  Long story, but tl;dr is they use dy = polynomial(log10(lambda))
    3. generalized a bit the function log10() so that we can use different transformations.

--- snpitdispenser.py
+++ snpitdispenser.py
@@ -56,25 +56,25 @@ def horner3(c, x, y, dl):
     
 
 @nb.njit(parallel=False)
-def relative_trace(Cijk, Dijk, xtrc, ytrc, wtran):
+def relative_trace(Cijk, Dijk, x0, y0, wtran):
     ''' compute the position along the trace.
 
     I believe this is equivalent to xarr and yarr in VM's code
     '''
     
-    npix = len(xtrc)
+    npix = len(x0)
     nlam = len(wtran)
 
     dx = np.empty((npix, nlam), dtype=float)
     dy = np.empty((npix, nlam), dtype=float)
     
     for i in range(npix):
-        xt = xtrc[i]
-        yt = ytrc[i]
+        xx0 = x0[i]
+        yy0 = y0[i]
 
         # Vihang calls these "yarr" and "xarr"
-        dely = horner3(Cijk, xt, yt, wtran)
-        delx = horner3(Dijk, xt, yt, dely)
+        dely = horner3(Cijk, xx0, yy0, wtran)
+        delx = horner3(Dijk, xx0, yy0, dely)
         
         # save results
         dy[i] = dely
@@ -82,27 +82,59 @@ def relative_trace(Cijk, Dijk, xtrc, ytrc, wtran):
 
     return dx, dy
 
-@nb.njit(parallel=False)
-def dispersion(dDijk, xtrc, ytrc, dl):
-    ''' compute the dispersion (ie. dlambda/dy) '''
-    
-    nd = dDijk.shape[0]
-    
-    npix = len(xtrc)
-    nlam = len(dl)
+@nb.njit
+def dispersion(dCijk, Cijk, dDijk, x0, y0, wtran):
+    npix = len(x0)
+    nlam = len(wtran)
 
-    dydl = np.empty((npix, nlam), dtype=float)
+    dwdr = np.empty((npix, nlam), dtype=float)
+
+    for i in range(npix):
+        xx0 = x0[i]
+        yy0 = y0[i]
+
+        # compute the position
+        dely = horner3(Cijk, xx0, yy0, wtran)
+        
+        # compute the derivatives        
+        dydl = horner3(dCijk, xx0, yy0, wtran)
+        dxdy = horner3(dDijk, xx0, yy0, dely)
+        
+        # use chain rule
+        dwdr[i] = 1/(dydl*np.sqrt(dxdy**2+1))
+        
+    return dwdr
+
+
+class LogTransformer:
+    ln10 = 2.302585092994046
+
+    def __init__(self, lam0):
+        self.lam0 = lam0
+
+    def evaluate(self, lam):
+        return np.log10(lam/self.lam0)
+
+    def invert(self, w):
+        return self.lam0 * 10**w
+
+    def deriv(self, lam):
+        return lam*self.ln10
     
-    for i in nb.prange(npix):
-        xt = xtrc[i]
-        yt = ytrc[i]
-        dely = np.full(nlam, horner2_scalar(dDijk[-1, :, :], xt, yt))
-        for ii in range(nd-2, -1, -1):
-            dely = dely*dl + horner2_scalar(dDijk[ii, :, :], xt, yt)
+class LinearTransformer:
+    
+    def __init__(self, lam0):
+        self.lam0 = lam0
 
-        dydl[i] = dely
-    return dydl
+    def evaluate(self, lam):
+        return lam-self.lam0
 
+    def invert(self, w):
+        return lam+self.lam0
+
+    def deriv(self, lam):
+        return 1.
+    
 
 class SNPITDisperser:
     def __init__(self, conffile):
@@ -114,8 +146,16 @@ class SNPITDisperser:
         with open(self.conffile, 'r') as fp:
             cfg = yaml.load(fp, Loader=yaml.FullLoader)
 
+        # fetch the meta data
         self.meta = cfg['meta']
+
+        # set the detector model
         self.detector = cfg['detector_model']
+
+        # change some units
+        self.detector['pixel_scale'] /= 3600.
+        
+        # set the optical model
         self.optical = {}
         self.optical['wl_min'] = cfg['optical_model']['wl_min']
         self.optical['wl_max'] = cfg['optical_model']['wl_max']
@@ -123,29 +163,54 @@ class SNPITDisperser:
         self.optical['wl_transform'] = cfg['optical_model']['wl_transform']
         self.optical['orders'] = {}
         for order, data in cfg['optical_model']['orders'].items():
-            self.optical['orders'][order] = {'xij': np.asarray(data['xmap_ij_coeff']),
-                                            'yij': np.asarray(data['ymap_ij_coeff']),
-                                            'crv': np.asarray(data['crv_ijk_coeff']),
-                                            'ids': np.asarray(data['ids_ijk_coeff'])}
             
+            
+            Xij = np.asarray(data['xmap_ij_coeff'])
+            Yij = np.asarray(data['ymap_ij_coeff'])
+            Cijk = np.asarray(data['ids_ijk_coeff'])
+            Dijk = np.asarray(data['crv_ijk_coeff'])
+
+            if Xij.all() and Yij.all() and Cijk.all() and Dijk.all():
+                
+                # save the results
+                self.optical['orders'][order] = {'Xij': Xij,
+                                                'Yij': Yij,
+                                                'Cijk': Cijk,
+                                                'dCijk': self.deriv_coeffs(Cijk),
+                                                'Dijk': Dijk,
+                                                'dDijk': self.deriv_coeffs(Dijk)}
+
+            
+        transform = self.optical['wl_transform'].lower()
+        if transform == 'log':
+            self.lam_transformer = LogTransformer(self.optical['wl_reference'])
+        elif transform == 'linear':
+            self.lam_transformer = LinearTransformer(self.optical['wl_reference'])
+        else:
+            raise NotImplementedError(f'Invalid transform {transform}')
+            
+    @staticmethod
+    def deriv_coeffs(M):
+        n = M.shape[0]
+        ii = np.arange(1, n, dtype=float)
+        dM = M[1:, :, :]*ii[:, np.newaxis, np.newaxis]
+        return dM
                 
     def sca_to_fpa(self, xsca, ysca, sca):
-        dx = self.detector['crpix1']-xsca # WHY NEGATIVE?!?!?
+        dx = self.detector['crpix1']-xsca # WHY NEGATIVE?
         dy = ysca-self.detector['crpix2']
 
         xcen, ycen = self.detector['xy_centers'][sca]
- 
-        
-        xfpa = (xcen*self.detector['plate_scale'] + dx)*self.detector['pixel_scale']/3600.
-
-        yfpa = (ycen*self.detector['plate_scale'] + dy)*self.detector['pixel_scale']/3600.
+         
+        xfpa = (xcen*self.detector['plate_scale'] + dx)*self.detector['pixel_scale']
+        yfpa = (ycen*self.detector['plate_scale'] + dy)*self.detector['pixel_scale']
 
         return xfpa, yfpa
 
     def fpa_to_mpa(self, xfpa, yfpa, order):
 
-        xmpa = horner2_vector(self.optical['orders'][order]['xij'], xfpa, yfpa)
-        ympa = horner2_vector(self.optical['orders'][order]['yij'], xfpa, yfpa)
+        xmpa = horner2_vector(self.optical['orders'][order]['Xij'], xfpa, yfpa)
+        ympa = horner2_vector(self.optical['orders'][order]['Yij'], xfpa, yfpa)
         
         return xmpa, ympa
 
@@ -160,29 +225,40 @@ class SNPITDisperser:
         xoff = (xmpa - xcen)*self.detector['plate_scale']
         yoff = (ympa - ycen)*self.detector['plate_scale']
 
-        xsca = self.detector['crpix1'] - xoff
+        xsca = self.detector['crpix1'] - xoff # WHY NEGATIVE?
         ysca = self.detector['crpix2'] + yoff
 
         return xsca, ysca
 
 
+    def dispersion(self, x0, y0, lam, sca, order='1'):
+        # coordinates will be 1d with N terms
+        x0 = np.atleast_1d(x0)
+        y0 = np.atleast_1d(y0)
+        lam = np.atleast_1d(lam)
 
-    def transform_wavelength(self, ww, inverse=False):
-        transform = self.optical['wl_transform']
-        reference = self.optical['wl_reference']
-        if transform == 'log':
-            if inverse:
-                return reference*(10.**ww)
-            else:
-                return np.log10(ww/reference)
-            
-        elif transform == 'linear':
-            if inverse:
-                return ww+reference
-            else:
-                return ww-reference
-        else:
-            raise NotImplementedError(f"Invalid transformation {transform}")
+        # convert SCA coordinates to FPA coordinates
+        xfpa, yfpa = self.sca_to_fpa(x0, y0, sca)
+
+        # transform wavelengths
+        wtran = self.lam_transformer.evaluate(lam)
+        
+        # compute the derivative
+        dwdt = dispersion(self.optical['orders'][order]['dCijk'],
+            self.optical['orders'][order]['Cijk'],
+            self.optical['orders'][order]['dDijk'],
+            xfpa, yfpa, wtran)
+        
+        # the derivative is dwtrans/dtheta.  so gotta transform
+        # wtrans back to lambda and theta to pixel, so compute those Jacobians.
+        dldw = self.lam_transformer.deriv(lam)
+        drdt = self.detector['plate_scale']
+
+        # we only want the positive side of things
+        dldr = np.abs(dwdt)*dldw/drdt
+        
+        return dldr
+
         
     def disperse(self, x0, y0, lam, sca, order='1'):
 
@@ -206,12 +282,14 @@ class SNPITDisperser:
         xfpa, yfpa = self.sca_to_fpa(x0, y0, sca)
                 
         # transform wavelengths
-        wtran = self.transform_wavelength(lam)
+        wtran = self.lam_transformer.evaluate(lam)
+        
+        #wtran = self.transform_wavelength(lam)
             
         # compute the relative trace
-        dx, dy = relative_trace(self.optical['orders'][order]['ids'],
-            self.optical['orders'][order]['crv'], xfpa, yfpa, wtran)
-
+        dx, dy = relative_trace(self.optical['orders'][order]['Cijk'],
+            self.optical['orders'][order]['Dijk'], xfpa, yfpa, wtran)
+            
         # apply trace.  But need the MPA coordinates
         xmpa, ympa = self.fpa_to_mpa(xfpa, yfpa, order)
         
