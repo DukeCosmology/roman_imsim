1c6a5a450d5dfa3b436c23ffe466ea18514f1356
Arun Kannawadi <arun.kannawadi@duke.edu>
2026-02-15 15:29:04 -0500

    Stop importing optical_model_utils automatically after reorg

--- roman_imsim/__init__.py
+++ roman_imsim/__init__.py
@@ -4,13 +4,11 @@ try:
     disable_implicit_threading()
 except ImportError:
     pass
-
 from .bandpass import *
 from .detector_physics import *
 
 # Import core modules for public use
 from .obseq import *
-from .optical_model_utils import *
 from .photonOps import *
 from .psf import *
 from .sca import *
