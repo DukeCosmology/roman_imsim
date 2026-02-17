1e308864e6b724084ba42809faa55de9643036c7
HyeongHan <50063544+HyeongHan@users.noreply.github.com>
2026-02-18 05:32:31 +0900

    Refactor date_test.py for clarity and functionality

--- tests/date_test.py
+++ tests/date_test.py
@@ -1,15 +1,10 @@
-import sys
 import yaml
 import galsim
 import galsim.config
 import galsim.roman as roman
 import logging
-import numpy as np
 from astropy.time import Time
-
 import roman_imsim
-import roman_imsim.obseq
-
 
 # --- read yaml ---
 with open("config/was.yaml", "r") as f:
@@ -27,13 +22,7 @@ logger = logging.getLogger("date_test")
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
-# Sanity check: do we even have the input block?
-#print("input keys:", list(base.get("input", {}).keys()))
-#print("obseq_data:", base.get("input", {}).get("obseq_data"))
-
-# --- THIS is the missing step ---
-galsim.config.ProcessInput(base, logger=logger)  # builds base['_input_objs'] :contentReference[oaicite:4]{index=4}
-
+galsim.config.ProcessInput(base, logger=logger)  
 req = {"SCA": int, "filter": str, "mjd": float, "exptime": float}
 
 opt = {
@@ -62,15 +51,11 @@ extra_ignore = [
     "random_seed"
 ]
 
-# Now your GetAllParams should be able to resolve mjd via ObSeqData -> GetInputObj
 params = galsim.config.GetAllParams(
     config, base, req=req, opt=opt, ignore=extra_ignore
 )[0]
 mjd = params["mjd"]
-#mjd = 60300
 date = Time(mjd, format="mjd").to_datetime()
-#print (mjd)
-#print ("DATE!! ", date)
 
 wcs_out = galsim.config.BuildWCS(config, 'wcs', base, logger=logger)
 wcs = wcs_out[0] if isinstance(wcs_out, tuple) else wcs_out
@@ -81,11 +66,8 @@ ysize = config.get("ysize", config.get("size", roman.n_pix))
 im = galsim.ImageF(xsize, ysize, wcs=wcs)
 
 world_pos = wcs.toWorld(im.true_center)
-sky_level = roman.getSkyLevel(base['bandpass'], world_pos=world_pos) #, date=date)
+sky_level = roman.getSkyLevel(base['bandpass'], world_pos=world_pos)
 print("before sky_level =", sky_level)
 
 sky_level = roman.getSkyLevel(base['bandpass'], world_pos=world_pos, date=date)
 print("after sky_level =", sky_level)
-
-
-
