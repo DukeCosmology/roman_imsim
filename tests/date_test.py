e673896b4d13302f8b611e04001a5d8b07f2be27
HyeongHan <50063544+HyeongHan@users.noreply.github.com>
2026-02-18 05:45:46 +0900

    Refactor date_test.py for code consistency

--- tests/date_test.py
+++ tests/date_test.py
@@ -38,16 +38,14 @@ extra_ignore = [
     "nobjects",
     "wcs",
     "bandpass",
-    "random_seed"
+    "random_seed",
 ]
 
-params = galsim.config.GetAllParams(
-    config, base, req=req, opt=opt, ignore=extra_ignore
-)[0]
+params = galsim.config.GetAllParams(config, base, req=req, opt=opt, ignore=extra_ignore)[0]
 mjd = params["mjd"]
 date = Time(mjd, format="mjd").to_datetime()
 
-wcs_out = galsim.config.BuildWCS(config, 'wcs', base)
+wcs_out = galsim.config.BuildWCS(config, "wcs", base)
 wcs = wcs_out[0] if isinstance(wcs_out, tuple) else wcs_out
 xsize = config.get("xsize", config.get("size", roman.n_pix))
 ysize = config.get("ysize", config.get("size", roman.n_pix))
@@ -55,8 +53,8 @@ ysize = config.get("ysize", config.get("size", roman.n_pix))
 # Dummy image just to get true_center in pixel coordinates
 im = galsim.ImageF(xsize, ysize, wcs=wcs)
 world_pos = wcs.toWorld(im.true_center)
-sky_level = roman.getSkyLevel(base['bandpass'], world_pos=world_pos)
+sky_level = roman.getSkyLevel(base["bandpass"], world_pos=world_pos)
 print("before sky_level =", sky_level)
 
-sky_level = roman.getSkyLevel(base['bandpass'], world_pos=world_pos, date=date)
+sky_level = roman.getSkyLevel(base["bandpass"], world_pos=world_pos, date=date)
 print("after sky_level =", sky_level)
