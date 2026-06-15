e06feef33d27cc50ab3f224930e7006035cd6291
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2026-06-15 12:25:22 -0400

    Please flake8

--- converter.py
+++ converter.py
@@ -9,7 +9,10 @@ logger = logging.getLogger()
 builder = RomanASDFBuilder()
 builder.include_raw_header = False
 
-fname_path = "/Users/ajk107/code/roman/roman_imsim/roman_imsim_testdata/output/RomanTDS_prism/images/simple_model/Roman_TDS_simple_model_test_SNPrism_4191_4.fits"
+fname_path = ("/Users/ajk107/code/roman/roman_imsim/roman_imsim_testdata/"
+              "output/RomanTDS_prism/images/simple_model/"
+              "Roman_TDS_simple_model_test_SNPrism_4191_4.fits"
+              )
 visit = 4191
 base = {"input": {"obseq_data": {"visit": visit}}}
 config = {}
